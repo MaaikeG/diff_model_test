fa5f5f2cc113cfc7270e6097ca3170317859a0b5
maaig92 <maaig92@mi.fu-berlin.de>
2022-11-15 18:45:48 +0100

    bug is fixed

--- train.py
+++ train.py
@@ -1,25 +1,7 @@
-from random import random
-import random
-import torch
-import matplotlib.pyplot as plt
-from plotting import plot_samples
-
-
-def get_loss(model, loss_fn, x_0, t_s):
-    x_t, noise = model.apply_noise(x_0, t_s)
-
-    predicted_noise = model.forward(x_t, t_s)
-
-    return loss_fn(predicted_noise, noise)
-
-
-def batch_iteration(model, loss_fn, optimizer, x_0):
+def batch_iteration(model, x_0, loss_fn, optimizer):
     model.zero_grad()
 
-    t = random.randint(0, model.T-1)
-    t_s = torch.full([x_0.shape[0]], t).to(x_0.device)  
-
-    loss = get_loss(model, loss_fn, x_0, t_s)
+    loss = model.loss(x_0, loss_fn)
     loss.backward()
     optimizer.step()
 
@@ -37,7 +19,7 @@ def train(model, loss_fn, data_loader, optimizer, n_iterations, scheduler=None, 
 
         for batch_idx, batch in enumerate(data_loader):
 
-            loss = batch_iteration(model, loss_fn, optimizer, batch[0].to(device))
+            loss = batch_iteration(model, batch[0].to(device), loss_fn, optimizer)
             loss.detach()
 
             iteration_loss += loss
